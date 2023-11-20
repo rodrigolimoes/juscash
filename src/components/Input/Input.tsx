7a5c9321e02d23789babd53ca0b404ce8879e3ec
Rodrigo Limões <rolimoes@gmail.com>
2023-11-20 18:19:38 -0300

    feat: Adding input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,62 @@
+import React, { ComponentProps, FC, ReactNode } from "react";
+import "./style.css";
+import classNames from "classnames";
+
+interface InputStateProps {
+  /**
+   * If true, the input will indicate an error
+   */
+  error?: boolean;
+  /**
+   * End adornment for this component
+   */
+  endAdornment?: ReactNode;
+  /**
+   * Text to indicate some help to the user
+   */
+  helperText?: string;
+}
+interface InputDispatchProps {}
+
+type InputProps = InputStateProps &
+  InputDispatchProps &
+  ComponentProps<"input">;
+
+/**
+ * Render an input field
+ * @param endAdornment End adornment for this component
+ * @param error If true, the input will indicate an error
+ * @param helperText Text to indicate some help to the user
+ * @param props Input props
+ * @constructor
+ * @example
+ * <Input type="password" placeholder="Password" endAdornment={<Eye />}/>
+ */
+const Input: FC<InputProps> = ({
+  endAdornment,
+  error,
+  helperText,
+  ...props
+}) => {
+  const inputStyle = classNames("input", {
+    "input-end-adornment": !!endAdornment,
+    "input-error": !!error,
+  });
+  const helperTextStyle = classNames("helper-text", {
+    "helper-text-error": !!error,
+  });
+
+  return (
+    <div>
+      <div className="relative flex items-center justify-end">
+        <input className={inputStyle} {...props} />
+        {endAdornment && (
+          <div className="absolute end-adornment">{endAdornment}</div>
+        )}
+      </div>
+      {helperText && <span className={helperTextStyle}>{helperText}</span>}
+    </div>
+  );
+};
+
+export default Input;
