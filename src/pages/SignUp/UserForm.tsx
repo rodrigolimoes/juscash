d575e73719b56da21bca04c3cdb11469bb6285aa
Rodrigo Limões <rolimoes@gmail.com>
2023-11-21 16:53:15 -0300

    feat: Adding useShowPassword hook; Adding functionality to show password

--- src/pages/SignUp/UserForm.tsx
+++ src/pages/SignUp/UserForm.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
-import { Eye } from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
+import { useShowPassword } from "../../hooks/useShowPassword";
 
 interface UserFormStateProps {}
 interface UserFormDispatchProps {}
@@ -9,23 +10,49 @@ interface UserFormDispatchProps {}
 type UserFormProps = UserFormStateProps & UserFormDispatchProps;
 
 const UserForm: FC<UserFormProps> = () => {
+  const [showPassword, handleClickShowPassword] = useShowPassword(false);
+  const [showConfirmPassword, handleClickShowConfirmPassword] =
+    useShowPassword(false);
+
   return (
     <form className="flex flex-col justify-start gap-16">
       <div className="flex flex-col gap-4">
-        <span className="text-primary">Seu Nome completo: *</span>
+        <label className="text-primary">Seu Nome completo: *</label>
         <Input className="w-full" type="text" />
       </div>
       <div className="flex flex-col gap-4">
-        <span className="text-primary">E-mail: *</span>
+        <label className="text-primary">E-mail: *</label>
         <Input className="w-full" type="email" />
       </div>
       <div className="flex flex-col gap-4">
-        <span className="text-primary">Senha: *</span>
-        <Input className="w-full" type="password" endAdornment={<Eye />} />
+        <label className="text-primary">Senha: *</label>
+        <Input
+          className="w-full"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <div
+              className="cursor-pointer text-secondary"
+              onClick={handleClickShowPassword}
+            >
+              {showPassword ? <EyeOff /> : <Eye />}
+            </div>
+          }
+        />
       </div>
       <div className="flex flex-col gap-4">
-        <span className="text-primary">Confirme sua senha: *</span>
-        <Input className="w-full" type="password" endAdornment={<Eye />} />
+        <label className="text-primary">Confirme sua senha: *</label>
+        <Input
+          className="w-full"
+          type={showConfirmPassword ? "text" : "password"}
+          endAdornment={
+            <div
+              className="cursor-pointer text-secondary"
+              onClick={handleClickShowConfirmPassword}
+            >
+              {showConfirmPassword ? <EyeOff /> : <Eye />}
+            </div>
+          }
+        />
       </div>
       <div>
         <span className="float-right text-sm">
