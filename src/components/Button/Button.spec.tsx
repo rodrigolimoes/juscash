d5222daf41b86964e819c79deb0b007ebfb5cfac
Rodrigo Limões <rolimoes@gmail.com>
2023-11-21 14:46:14 -0300

    test: Ensure render a secondary button

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -54,5 +54,18 @@ describe("Button", () => {
       expect(getByText("Create")).toBeDefined();
       expect(button.className).toEqual("button button-outlined-success");
     });
+
+    it("should render a secondary button", () => {
+      const { getByText, container } = render(
+        <Button variant="outlined" color="secondary">
+          Create
+        </Button>
+      );
+
+      const button = container.querySelector("button");
+
+      expect(getByText("Create")).toBeDefined();
+      expect(button.className).toEqual("button button-outlined-secondary");
+    });
   });
 });
