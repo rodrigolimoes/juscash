1b93fb4b6b2ea858e49be4bcee2f8aa49c3d9f7e
Rodrigo Limões <rolimoes@gmail.com>
2023-11-21 14:42:36 -0300

    test: Ensure render a success button

---
+++ src/components/Button/Button.spec.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Button", () => {
+  describe("contained", () => {
+    it("should render a success button", () => {
+      const { getByText, container } = render(
+        <Button variant="contained" color="success">
+          Create
+        </Button>
+      );
+
+      const button = container.querySelector("button");
+
+      expect(getByText("Create")).toBeDefined();
+      expect(button.className).toEqual("button button-contained-success");
+    });
+  });
+});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,53 @@
+import React, { ComponentProps, FC, ReactNode } from "react";
+import "./style.css";
+import classNames from "classnames";
+
+type Variant = "contained" | "outlined";
+type Color = "success" | "secondary" | "info";
+
+interface ButtonStateProps {
+  color?: Color;
+  variant?: Variant;
+  icon?: ReactNode;
+}
+interface ButtonDispatchProps {}
+
+type ButtonProps = ButtonStateProps &
+  ButtonDispatchProps &
+  ComponentProps<"button">;
+
+/**
+ * Render a Button
+ * @param children Button content
+ * @param color Button color
+ * @param variant Button variant
+ * @param className Override or extend the styles applied to the component
+ * @param icon Icon to render on button
+ * @param props Button props
+ * @constructor
+ *
+ * @example
+ * <Button variant="outlined" color="success">Create</Button>
+ */
+const Button: FC<ButtonProps> = ({
+  children,
+  color = "info",
+  variant = "contained",
+  className,
+  icon,
+  ...props
+}) => {
+  const buttonStyle = classNames("button", className, {
+    [`button-${variant}-${color}`]: true,
+    "button-icon": !!icon,
+  });
+
+  return (
+    <button className={buttonStyle} {...props}>
+      {icon && <>{icon}</>}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
