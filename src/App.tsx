b82f710fce0938fa492d7472743703d1e5544dc7
Rodrigo Limões <rolimoes@gmail.com>
2023-11-22 09:48:05 -0300

    feat: Adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import SignUp from "./pages/SignUp/SignUp";
+import React, { lazy, Suspense } from "react";
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
+const Login = lazy(() => import("./pages/Login/Login"));
 
 function App() {
-  return <SignUp />;
+  return (
+    <Suspense>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route element={<RequireAuth />}>
+                  <Route path="/" element={<>Home</>} />
+                </Route>
+              </Routes>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
 }
 
 export default App;
