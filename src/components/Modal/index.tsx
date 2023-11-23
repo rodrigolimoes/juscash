678972f5c7e44a9984ef4272aa7944eb3bbde9d2
Rodrigo Limões <rolimoes@gmail.com>
2023-11-23 20:07:15 -0300

    fix router bug

--- src/components/Modal/index.tsx
+++
@@ -1,6 +0,0 @@
-import Modal from "./Modal";
-import Header from "./Composition/Header";
-import Footer from "./Composition/Footer";
-import Body from "./Composition/Body";
-
-export { Modal, Header, Footer, Body };
--- src/pages/Login/Form/LoginFormDto.ts
+++
@@ -1,6 +0,0 @@
-import zod from "zod";
-
-export const loginFormDto = zod.object({
-  email: zod.string().email("E-mail inválido").min(1, "Campo obrigatório"),
-  password: zod.string().min(1, "Campo obrigatório"),
-});
